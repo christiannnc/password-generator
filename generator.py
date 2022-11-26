d667bad2fd1b9c400bf33c0c1355cdfb916f88ad
Christian Cox <57050814+christiannnc@users.noreply.github.com>
2022-11-25 23:19:03 -0500

    Update generator.py

--- generator.py
+++ generator.py
@@ -46,7 +46,3 @@ def generate_pass():
     return generated_pass
 
 print("\nHere's your password: " + generate_pass())
-
-# this program is very simple and not necessarily secure
-# much more logic could be added to ensure things like repetitive characters
-# and missing requirements are prevented, but this a good basic password generator
